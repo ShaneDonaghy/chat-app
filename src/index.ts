d48696578c7ff2ca686f62eb836515937268ffce
Shane Donaghy <shane@MAC-4228CE.station>
2025-04-16 11:54:12 +0100

    interfaces and concrete classes for in memory database (array) implemented

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { logger } from 'hono/logger';
 import { timing} from "hono/timing";
 
 const app = new Hono();
+
 app.use("*", timing());
 app.use("*", logger());
+
 console.log(Bun.env.TEST);
+console.log(Bun.env.AI);
+
 app.get('/', (c) => c.json({"message": "Hello Hono!"}));
 
 export default app;
